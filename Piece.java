ec951761ee8e4747a495ad7c603b37e7540975a0
ryan570 <brownryan570@gmail.com>
2018-11-30 20:02:23 -0500

    Pawn collision mechanics

--- Piece.java
+++ Piece.java
@@ -19,7 +19,7 @@ public class Piece extends ImageView {
         fitWidthProperty().bind(position.widthProperty());
         fitHeightProperty().bind(position.heightProperty());
 
-        switch(type) {
+        switch (type) {
             case PAWN:
                 if (color == Color.WHITE) setImage(new Image("chess/sprites/white-pawn.png"));
                 else setImage(new Image("chess/sprites/black-pawn.png"));
@@ -44,7 +44,8 @@ public class Piece extends ImageView {
                 if (color == Color.WHITE) setImage(new Image("chess/sprites/white-king.png"));
                 else setImage(new Image("chess/sprites/black-king.png"));
                 break;
-            default: break;
+            default:
+                break;
         }
 
         this.type = type;
@@ -56,7 +57,7 @@ public class Piece extends ImageView {
     public void move(Position from, Position to) {
         boolean valid = checkValidMove(from, to);
         boolean noCollisions = checkCollisions(from, to);
-        if (valid && noCollisions && unlocked) {
+        if ((valid && noCollisions && unlocked) || (type == Type.PAWN && valid && unlocked)) {
             position.removePiece();
             setPosition(to);
             position.setPiece(this);
@@ -89,7 +90,8 @@ public class Piece extends ImageView {
             int ddX = colDiff / Math.abs(colDiff), ddY = rowDiff / Math.abs(rowDiff);
 
             for (int i = 1; i < Math.abs(colDiff); i++) {
-                if (Board.findPosition(Board.columns[currentCol + (i * ddX) - 1], Board.rows[8 - (currentRow + (i * ddY))]).hasPiece()) return false;
+                if (Board.findPosition(Board.columns[currentCol + (i * ddX) - 1], Board.rows[8 - (currentRow + (i * ddY))]).hasPiece())
+                    return false;
             }
         }
 
@@ -112,11 +114,9 @@ public class Piece extends ImageView {
     }
 
     private boolean checkFinal(Position to) {
-        if (to.getPiece().getColor() == color) {
-            return false;
-        } else {
-            return true;
-        }
+        if (to.hasPiece()) {
+            return to.getPiece().getColor() != color;
+        } else return false;
     }
 
     public void setPosition(Position pos) {
@@ -133,14 +133,13 @@ public class Piece extends ImageView {
         int colDiff = futureCol - currentCol;
 
         switch (type) {
-            //TODO: FIX PAWN COLLISION MECHANICS
             case PAWN:
                 if (to.getColumn() == from.getColumn()) {
+                    if (to.hasPiece()) return false;
                     if ((currentRow == 2 && color == Color.WHITE) || (currentRow == 7 && color == Color.BLACK)) {
                         if (rowDiff <= 2 && rowDiff >= -2) return true;
-                    } else if (rowDiff == 1 && color == Color.WHITE) return true;
-                    else if (rowDiff == -1 && color == Color.BLACK) return true;
-                }
+                    } else if (Math.abs(rowDiff) == 1) return true;
+                } else if (checkFinal(to) && Math.abs(colDiff) == 1 && ((rowDiff == 1 && color == Color.WHITE) || (rowDiff == -1 && color == Color.BLACK))) return true;
                 break;
             case KNIGHT:
                 if (Math.abs(colDiff) == 2 && Math.abs(rowDiff) == 1) return true;
@@ -157,8 +156,10 @@ public class Piece extends ImageView {
                 else if ((currentCol == futureCol || currentRow == futureRow)) return true;
                 break;
             case KING:
-                if ((Math.abs(rowDiff) == Math.abs(colDiff) && Math.abs(colDiff) == 1 && Math.abs(rowDiff) == 1)) return true;
-                    else if (((currentCol == futureCol || currentRow == futureRow) && (Math.abs(colDiff) == 1 || Math.abs(rowDiff) == 1))) return true;
+                if ((Math.abs(rowDiff) == Math.abs(colDiff) && Math.abs(colDiff) == 1 && Math.abs(rowDiff) == 1))
+                    return true;
+                else if (((currentCol == futureCol || currentRow == futureRow) && (Math.abs(colDiff) == 1 || Math.abs(rowDiff) == 1)))
+                    return true;
                 break;
             default:
                 break;
